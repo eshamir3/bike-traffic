228e7462853659767a67320d1c7e2a825feca3bd
eshamir3 <156357368+eshamir3@users.noreply.github.com>
2025-05-16 20:06:12 -0700

    Update map.js

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // Set your Mapbox access token here
-mapboxgl.accessToken = 'YOUR_ACCESS_TOKEN_HERE';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXNoYW1pcjMiLCJhIjoiY21hcm5hMGd3MDhnazJtcHl6c2I0cGl5bCJ9.7I5fj9mgwkHHL0ImMOiTlQ';
 
 // Initialize the map
 const map = new mapboxgl.Map({
